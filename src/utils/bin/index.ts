ea9a388c09afdd28756f03785d1d9645dcbba84e
Andrew Carvajal <andrew.carvajal@me.com>
2022-05-26 20:19:24 -0400

    Replaced everything a third time for a third solution using a jQuery library

--- src/utils/bin/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './utils';
-export * from './social';
-export { default as weather } from './weather';
-export { default as about } from './about';
-export { default as projects } from './projects';
