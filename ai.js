20985292b1204bc77d3edcd953abcaebfef70ed5
Andrew Carvajal <andrew.carvajal@me.com>
2022-12-19 15:33:11 -0500

    Reverted to previous version

--- ai.js
+++ ai.js
@@ -1,4 +1,4 @@
-import { Configuration, OpenAIApi } from "openai"
+const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
     apiKey: "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",
 });
